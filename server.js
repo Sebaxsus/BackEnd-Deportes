de7d75c1d0a866a3b55a1930e8dd97fbf0f9f99c
Sebaxsus <sebastiangarcia1198@gmail.com>
2022-12-22 19:22:41 -0500

    Back end with auth token and env keys

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-//require ('dotenv').config()
+require('dotenv').config()
 const express=require('express')
-const mysql=require('mysql')
+const mysql2=require('mysql2')
 const myconn=require('express-myconnection')
 const routes=require('./routes')
 const cors= require('cors')
+const jwt = require('jsonwebtoken')
+
 
 const app=express()
+
 app.use(cors())
 
 app.set('port',9000)
 
 const dbOptions={
+    database:process.env.DB_NAME || 'ciclo4mtic',
     host:process.env.DB_HOST || 'localhost',
-    port:process.env.DB_PORT || '3306',
-    user:process.env.DB_USER || 'MinTic',
     password:process.env.DB_PWD || 'JsgrMc1',
-    database:process.env.DB_NAME || 'ciclo4mtic'
+    port:process.env.DB_PORT || '3306',
+    user:process.env.DB_USER || 'MinTic'
+    
+    
+    
 }
 
 /// middlewares
-app.use(myconn(mysql,dbOptions,'single'))
+app.use(myconn(mysql2,dbOptions,'single'))
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 
 /// routes
@@ -28,8 +35,52 @@ app.get('/',(req,res)=>{
     res.send('Welcome to my APP 2022')
 })
 
-app.use('/api',routes)
+app.use('/api',validacionToken,routes)
+
+app.get('/login',(req,res)=>{
+    res.send(`<html>
+       <head>
+          <title>Inicio de Sesión</title>
+       </head>
+       <body>
+          <form method="POST" action="/auth">
+             UserName: <input type="text" name="user"><br />
+             Password: <input type="password" name="password"><br/>
+             <input type="submit" value="Login" />
+          </form>
+       </body>
+    </html>`)
+  })
+
+app.post('/auth',(req,res)=>{
+    const {username,password} = req.body
+    const user={username: username}
+  
+    const accesToken= generarToken(user)
+    res.header('autorization',accesToken).json({
+      message: 'autenticado',
+      token: accesToken
+    })
+    localStorage.setItem("token", accesToken)
+    //localStorafe.getItem("token") conseguir
+  })
+  
+  function generarToken(user){
+    return jwt.sign(user,process.env.claveJwt+"",{expiresIn: '15m'})
+  }
  
+  function validacionToken(req, res, next){
+    const accesToken=req.headers['autorization']
+    if(!accesToken) res.send('Acceso Denegado')
+
+    jwt.verify(accesToken, process.env.claveJwt+"",(err,user)=>{
+        if(err){
+            res.send("Acceso Denegado, Token Invalido, Expirado o Inexistente")
+        }else{
+            next()
+        }
+    })
+  }
 
 app.listen(app.get('port'),()=>{
     console.log(`El puerto corre en: ${app.get('port')}`)
